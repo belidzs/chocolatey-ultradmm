0b97ba3713eef00aba9a2540202fa801b71a329e
Balázs Keresztury <b@belidzs.hu>
2019-02-27 01:28:35 +0100

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.ultradmm.com/release/1.0.5/setup.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'ultradmm*'
+
+  checksum      = 'dd3fb9abe9b838ccda4c600df14532231245dc8bb59d844dde697481647c6dc1'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
